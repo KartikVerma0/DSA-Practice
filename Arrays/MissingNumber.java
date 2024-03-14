d9aca9605b52e6549be4e2d130cf711e75390ff4
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-14 22:51:53 +0530

    Another solution added

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -11,23 +11,43 @@ package Arrays;
 
 public class MissingNumber {
     public static void main(String[] args) {
-        System.out.println(missingNumber(new int[] { 3, 0, 1 }));
+        System.out.println(missingNumber(new int[] { 3, 1, 0 }));
+    }
+
+    // static int missingNumber(int[] nums) {
+    // boolean[] isPresent = new boolean[nums.length + 1];
+
+    // for (int i = 0; i < nums.length; i++) {
+    // isPresent[nums[i]] = true;
+    // }
+    // int index = 0;
+    // for (int i = 0; i < isPresent.length; i++) {
+    // if (!isPresent[i]) {
+    // index = i;
+    // break;
+    // }
+
+    // }
+    // return index;
+    // }
+
+    static void swap(int[] nums, int i1, int i2) {
+        int tmp = nums[i1];
+        nums[i1] = nums[i2];
+        nums[i2] = tmp;
     }
 
     static int missingNumber(int[] nums) {
-        boolean[] isPresent = new boolean[nums.length + 1];
-
+        int pos = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            isPresent[nums[i]] = true;
-        }
-        int index = 0;
-        for (int i = 0; i < isPresent.length; i++) {
-            if (!isPresent[i]) {
-                index = i;
-                break;
+            while (nums[i] != i) {
+                if (nums[i] == nums.length) {
+                    pos = i;
+                    break;
+                }
+                swap(nums, nums[i], i);
             }
-
         }
-        return index;
+        return pos;
     }
 }
