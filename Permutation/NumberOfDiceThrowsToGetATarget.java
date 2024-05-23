ea30024ad99f9767afc657016dc544d2f93a9283
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-23 12:26:06 +0530

    New "Number of dice throws to get a target"

--- Permutation/NumberOfDiceThrowsToGetATarget.java
+++ Permutation/NumberOfDiceThrowsToGetATarget.java
@@ -1,12 +1,15 @@
 package Permutation;
 
+import java.util.ArrayList;
+
 /*
  * Number of ways to get a target
  */
 public class NumberOfDiceThrowsToGetATarget {
     public static void main(String[] args) {
         int target = 4;
-        System.out.println(helper(0, target, target));
+        // System.out.println(helper(0, target, target));
+        System.out.println(helperArrayList(0, target, target, new ArrayList<>()).toString());
     }
 
     static int helper(int processed, int unprocessed, int target) {
@@ -21,5 +24,23 @@ public class NumberOfDiceThrowsToGetATarget {
             ways += helper(processed + i, unprocessed - i, target);
         }
         return ways;
+    }
+
+    static ArrayList<Integer> helperArrayList(int processed, int unprocessed, int target,
+            ArrayList<Integer> processedList) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (processed == target) {
+            return processedList;
+        }
+        if (processed > target) {
+            return list;
+        }
+        for (int i = 1; i <= 6; i++) {
+            processedList.add(i);
+            list.addAll(helperArrayList(processed + i, unprocessed - i, target, processedList));
+            int index = processedList.indexOf(i);
+            processedList.remove(index);
+        }
+        return list;
     }
 }
